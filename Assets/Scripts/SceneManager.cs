da9c274498e3e692b433d62303a56d5deb6d978d
Dominic Aglialoro <domiaglialoro@gmail.com>
2023-01-21 19:25:50 -0800

    Other event controls

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,3 +1,4 @@
+using System;
 using SRXDCustomVisuals.Core;
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -16,6 +17,8 @@ public class SceneManager : MonoBehaviour {
     private bool spinningRight;
     private bool spinningLeft;
     private bool scratching;
+    private int eventIndex;
+    private double eventValue = 255d;
 
     private void Start() {
         postProcessingVolume.profile.TryGet(out bloomComponent);
@@ -35,47 +38,91 @@ public class SceneManager : MonoBehaviour {
             bloomComponent.intensity.value = bloom;
         
         GUILayout.Space(20f);
-        GUILayout.Label("Events");
+        GUILayout.Label("Note Events");
 
         if (GUILayout.Button("Hit Match"))
-            SendNoteHit(NoteIndex.HitMatch);
+            SendEventHit((int) NoteIndex.HitMatch);
         
         if (GUILayout.Button("Hit Tap"))
-            SendNoteHit(NoteIndex.HitTap);
+            SendEventHit((int) NoteIndex.HitTap);
         
         if (GUILayout.Button("Hit Beat"))
-            SendNoteHit(NoteIndex.HitBeat);
+            SendEventHit((int) NoteIndex.HitBeat);
         
         if (GUILayout.Button("Hit Spin Right"))
-            SendNoteHit(NoteIndex.HitSpinRight);
+            SendEventHit((int) NoteIndex.HitSpinRight);
         
         if (GUILayout.Button("Hit Spin Left"))
-            SendNoteHit(NoteIndex.HitSpinLeft);
+            SendEventHit((int) NoteIndex.HitSpinLeft);
         
         if (GUILayout.Button("Hit Scratch"))
-            SendNoteHit(NoteIndex.HitScratch);
+            SendEventHit((int) NoteIndex.HitScratch);
         
-        UpdateNoteHold(ref holding, GUILayout.Toggle(holding, "Holding"), NoteIndex.Hold);
-        UpdateNoteHold(ref beatHolding, GUILayout.Toggle(beatHolding, "Beat Holding"), NoteIndex.HoldBeat);
-        UpdateNoteHold(ref spinningRight, GUILayout.Toggle(spinningRight, "Spinning Right"), NoteIndex.HoldSpinRight);
-        UpdateNoteHold(ref spinningLeft, GUILayout.Toggle(spinningLeft, "Spinning Left"), NoteIndex.HoldSpinLeft);
-        UpdateNoteHold(ref scratching, GUILayout.Toggle(scratching, "Scratching"), NoteIndex.HoldScratch);
+        UpdateNoteHold(ref holding, GUILayout.Toggle(holding, "Holding"), (int) NoteIndex.Hold);
+        UpdateNoteHold(ref beatHolding, GUILayout.Toggle(beatHolding, "Beat Holding"), (int) NoteIndex.HoldBeat);
+        UpdateNoteHold(ref spinningRight, GUILayout.Toggle(spinningRight, "Spinning Right"), (int) NoteIndex.HoldSpinRight);
+        UpdateNoteHold(ref spinningLeft, GUILayout.Toggle(spinningLeft, "Spinning Left"), (int) NoteIndex.HoldSpinLeft);
+        UpdateNoteHold(ref scratching, GUILayout.Toggle(scratching, "Scratching"), (int) NoteIndex.HoldScratch);
+        
+        GUILayout.Space(20f);
+        GUILayout.Label("Other Events");
+        
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Index:");
+
+        string field = GUILayout.TextField(eventIndex.ToString(), GUILayout.Width(200f));
+
+        if (string.IsNullOrWhiteSpace(field))
+            eventIndex = 0;
+        else if (int.TryParse(field, out int newEventIndex))
+            eventIndex = Mathf.Clamp(newEventIndex, 0, 255);
+        
+        GUILayout.EndHorizontal();
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Value:");
+
+        field = GUILayout.TextField(eventValue.ToString("0.0#"), GUILayout.Width(200f));
+        
+        if (string.IsNullOrWhiteSpace(field))
+            eventValue = 0d;
+        else if (double.TryParse(field, out double newEventValue))
+            eventValue = Math.Clamp(newEventValue, 0d, 255d);
+        
+        GUILayout.EndHorizontal();
+        
+        if (GUILayout.Button("Hit"))
+            SendEventHit(eventIndex, eventValue);
+        
+        if (GUILayout.Button("On"))
+            SendEvent(VisualsEventType.On, eventIndex, eventValue);
+        
+        if (GUILayout.Button("Off"))
+            SendEvent(VisualsEventType.Off, eventIndex);
+        
+        if (GUILayout.Button("Set Control"))
+            SendEvent(VisualsEventType.ControlChange, eventIndex, eventValue);
     }
 
-    private static void SendNoteHit(NoteIndex index) {
+    private static void SendEventHit(int index, double value = 255d) {
         var visualsEventManager = VisualsEventManager.Instance;
         
-        visualsEventManager.SendEvent(new VisualsEvent(VisualsEventType.On, (int) index, 255d));
-        visualsEventManager.SendEvent(new VisualsEvent(VisualsEventType.Off, (int) index, 255d));
+        visualsEventManager.SendEvent(new VisualsEvent(VisualsEventType.On, index, value));
+        visualsEventManager.SendEvent(new VisualsEvent(VisualsEventType.Off, index, 255d));
+    }
+    
+    private static void SendEvent(VisualsEventType type, int index, double value = 255d) {
+        var visualsEventManager = VisualsEventManager.Instance;
+        
+        visualsEventManager.SendEvent(new VisualsEvent(type, index, value));
     }
 
-    private static void UpdateNoteHold(ref bool state, bool newState, NoteIndex index) {
+    private static void UpdateNoteHold(ref bool state, bool newState, int index) {
         var visualsEventManager = VisualsEventManager.Instance;
         
         if (!state && newState)
-            visualsEventManager.SendEvent(new VisualsEvent(VisualsEventType.On, (int) index, 255d));
+            visualsEventManager.SendEvent(new VisualsEvent(VisualsEventType.On, index, 255d));
         else if (state && !newState)
-            visualsEventManager.SendEvent(new VisualsEvent(VisualsEventType.Off, (int) index, 255d));
+            visualsEventManager.SendEvent(new VisualsEvent(VisualsEventType.Off, index, 255d));
 
         state = newState;
     }
